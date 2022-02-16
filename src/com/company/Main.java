f666971708d87b155fdec672414e8437d9b9234b
Lelya <maksimenkova.o00@mail.ru>
2022-02-16 19:35:57 +0300

    инит

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final int countOfCars;
+        double countOfMoney = 15.20;
+        boolean weatherIsSunny = true;
+
+        countOfMoney = 1;
+        countOfCars = 23;
+
+        System.out.println(countOfCars);
+        System.out.println(countOfMoney);
+        System.out.println(weatherIsSunny);
+    }
+}
---
+++ src/com/company/variables/Task1.java
@@ -0,0 +1,27 @@
+package com.company.variables;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int calendarDay = 15;
+        int calendarMonth = 2;
+        int calendarYear = 2002;
+
+        final double numberPi = 3.14;
+
+        int numberOfGuests;
+        numberOfGuests = 15;
+        int tableCups = numberOfGuests;
+        int tableForks = numberOfGuests;
+
+        double temperatureStorage = -3.6;
+
+    //    System.out.print(calendarDates);
+        System.out.print(".");
+        System.out.println(calendarYear);
+        System.out.println(numberPi);
+        System.out.println(numberOfGuests);
+        System.out.println(tableCups);
+        System.out.println(tableForks);
+        System.out.println(temperatureStorage);
+    }
+}
