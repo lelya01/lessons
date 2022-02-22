18efed451faaf368e88853571fbac596dcce2bea
Lelya <maksimenkova.o00@mail.ru>
2022-02-22 20:51:29 +0300

    Задание 6

--- src/com/company/task2/Task2.java
+++ src/com/company/task2/Task2.java
@@ -2,22 +2,37 @@ package com.company.task2;
 
 public class Task2 {
     public static void main(String[] args) {
-        int a = 7;
-        int b = 5;
-        int c = 1;
-        int f = 2;
-        int r = 5;
-        int x = 9;
-        int d = a + b + c;
-        int k = f + r + x;
-        if(d == k){
-            System.out.println("Счастливый билет");
+//        int a = 7;
+//        int b = 5;
+//        int c = 1;
+//        int f = 2;
+//        int r = 5;
+//        int x = 9;
+//        int d = a + b + c;
+//        int k = f + r + x;
+//        if(d == k){
+//            System.out.println("Счастливый билет");
+//            }
+//        else
+//            System.out.println("Несчастливый билет");
+        int a = 15;
+        int b = 10;
+        int c = 15;
+        int d = 19;
+        if (a == c){
+
+            if (b == d){
+                System.out.println("Можно вложить");
+
             }
-        else
-            System.out.println("Несчастливый билет");
-        }
+            else
+                System.out.println("Нельзя вложить");
+            }
 
         }
+    }
+
+
 
 
 
