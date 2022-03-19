f74281f1d6b6a09b9662dc2bf5907da7bc5a7af0
Lelya <maksimenkova.o00@mail.ru>
2022-03-20 00:37:29 +0300

    2.Задание1603(Массив)

--- src/com/company/Massive/homMassive1603.java
+++ src/com/company/Massive/homMassive1603.java
@@ -1,4 +1,13 @@
 package com.company.Massive;
-
+//Найти кол-во четных элементов
 public class homMassive1603 {
+    public static void main(String[] args) {
+        long []array = {22,93,2,6,75,13};
+
+        for(int i = 0; i < array.length; i++){
+            if(array[i] % 2 == 0){
+                System.out.println(array[i]);
+            }
+        }
+    }
 }
--- src/com/company/Massive/homeMassive1603.java
+++ src/com/company/Massive/homeMassive1603.java
@@ -1,8 +1,9 @@
 package com.company.Massive;
-
+//Вывести элементы массива в ряд
 public class homeMassive1603 {
     public static void main(String[] args) {
         int []array = {4,2,6,9,12,23,4,67,8};
+
          for(int i = 0; i < array.length; i++ ){
              System.out.println(array[i]);
          }
